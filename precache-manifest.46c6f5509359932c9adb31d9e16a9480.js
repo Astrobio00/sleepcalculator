90efe0a6d898243f4665f0003bd8aa411c79061f
Guilherme <ghghuihenrique@gmail.com>
2020-02-27 19:42:17 -0300

    Updates

---
+++ precache-manifest.46c6f5509359932c9adb31d9e16a9480.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83bc14e5578474d854a113029bf28947",
+    "url": "/sleepcalculator/index.html"
+  },
+  {
+    "revision": "3334fb5e44f4dc1bc54a",
+    "url": "/sleepcalculator/static/css/main.acae8ccf.chunk.css"
+  },
+  {
+    "revision": "aeb3c71438b6fbd07827",
+    "url": "/sleepcalculator/static/js/2.26569ba2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/sleepcalculator/static/js/2.26569ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3334fb5e44f4dc1bc54a",
+    "url": "/sleepcalculator/static/js/main.0e844cca.chunk.js"
+  },
+  {
+    "revision": "0f92dbc1325bd692cbae",
+    "url": "/sleepcalculator/static/js/runtime-main.5a5275fc.js"
+  }
+]);
